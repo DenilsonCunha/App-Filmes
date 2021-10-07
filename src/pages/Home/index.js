7fbd59a4049bcd547a68220b25e8330a63eb5d9c
Denilson Luciano <denilsonluciano6@gmail.com>
2021-10-07 09:56:20 -0300

    detalhes dos filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,153 +1,151 @@
-import React, { useState, useEffect } from 'react'
-import { ScrollView, ActivityIndicator } from 'react-native'
+import React, { useState, useEffect } from "react";
+import { ScrollView, ActivityIndicator } from "react-native";
 
-
-import { 
+import {
   Container,
-   SearchContainer,
-    Input,
-     SearchButton,
-      Title,
-       BannerButton,
-        Banner,
-         SliderMovie
-  } from './style'
+  SearchContainer,
+  Input,
+  SearchButton,
+  Title,
+  BannerButton,
+  Banner,
+  SliderMovie,
+} from "./style";
 
-import { Feather } from '@expo/vector-icons'
+import { Feather } from "@expo/vector-icons";
 
-import Header from '../../components/Header'
-import SliderItem from '../../components/Header/SliderItem'
+import Header from "../../components/Header";
+import SliderItem from "../../components/Header/SliderItem";
 
-import api, { key } from '../../services/api'
-import { getListMovies, randomBanner} from '../../utills/movies'
+import api, { key } from "../../services/api";
+import { getListMovies, randomBanner } from "../../utills/movies";
 
+import { useNavigation } from '@react-navigation/native'
 
-function Home(){
-
+function Home() {
   const [nowMovies, setnowMovies] = useState([]);
   const [popularMovies, setpopularMovies] = useState([]);
   const [topMovies, setTopMovies] = useState([]);
   const [bannerMovie, setBannerMovie] = useState({});
   const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-       let isActive = true;
-       const ac = new AbortController();
+  const navigation = useNavigation();
 
+  useEffect(() => {
+    let isActive = true;
+    const ac = new AbortController();
 
-       async function getMovies(){
-        const [nowData, popularData, topData] = await Promise.all([
-          api.get('/movie/now_playing', {
-            params: {
-              api_key: key,
-              language: 'pt-BR',
-              page: 1,
-            }
-          }),
-          api.get('/movie/popular', {
-            params: {
-              api_key: key,
-              language: 'pt-BR',
-              page: 1,
-            }
-          }),
-          api.get('/movie/top_rated', {
-            params: {
-              api_key: key,
-              language: 'pt-BR',
-              page: 1,
-            }
-          }),
-        ])
+    async function getMovies() {
+      const [nowData, popularData, topData] = await Promise.all([
+        api.get("/movie/now_playing", {
+          params: {
+            api_key: key,
+            language: "pt-BR",
+            page: 1,
+          },
+        }),
+        api.get("/movie/popular", {
+          params: {
+            api_key: key,
+            language: "pt-BR",
+            page: 1,
+          },
+        }),
+        api.get("/movie/top_rated", {
+          params: {
+            api_key: key,
+            language: "pt-BR",
+            page: 1,
+          },
+        }),
+      ]);
 
-  if(isActive){
-         const nowList = getListMovies(10, nowData.data.results);
-         const popularList = getListMovies(10, popularData.data.results);
-         const topList = getListMovies(10, topData.data.results);
+      if (isActive) {
+        const nowList = getListMovies(10, nowData.data.results);
+        const popularList = getListMovies(10, popularData.data.results);
+        const topList = getListMovies(10, topData.data.results);
 
-         setBannerMovie(nowData.data.results[randomBanner(nowData.data.results)])
-         setnowMovies(nowList);
-         setpopularMovies(popularList);
-         setTopMovies(topList);
-         setLoading(false);
+        setBannerMovie(
+          nowData.data.results[randomBanner(nowData.data.results)]
+        );
+        setnowMovies(nowList);
+        setpopularMovies(popularList);
+        setTopMovies(topList);
+        setLoading(false);
+      }
+    }
 
-        }
+    getMovies();
 
-        
+    return () => {
+      isActive = false;
+      ac.abort();
+    };
+  }, []);
 
-       }
-
-       getMovies();
-
-       return () => {
-         isActive = false;
-         ac.abort();
-       }
-
-
-  }, [])
-
-  if(loading){
-    return(
-      <Container>
-        <ActivityIndicator size="large" color="#FFF"/>
-      </Container>
-    )
+  function navegateDetailsPage(item) {
+    navigation.navigate('Detail', {id: item.id})
   }
 
+  if (loading) {
+    return (
+      <Container>
+        <ActivityIndicator size="large" color="#FFF" />
+      </Container>
+    );
+  }
 
-    return(
-        <Container>
-            <Header title="Prime Filmes"/>
-              <SearchContainer>
-                 <Input 
-               placeholder="Ex: Vingadores"
-                 />
-             <SearchButton>
-                 <Feather name="search" size={30} color="#FFF" />
-             </SearchButton>
-           </SearchContainer>
+  return (
+    <Container>
+      <Header title="Prime Filmes" />
+      <SearchContainer>
+        <Input placeholder="Ex: Vingadores" />
+        <SearchButton>
+          <Feather name="search" size={30} color="#FFF" />
+        </SearchButton>
+      </SearchContainer>
 
-           <ScrollView showsVerticalScrollIndicator={false}>
-             <Title>Em cartaz</Title>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Title>Em cartaz</Title>
 
-             <BannerButton>
-               <Banner
-                resizeMethod="resize"
-                 source={{ uri: `https://image.tmdb.org/t/p/original/${bannerMovie.poster_path}` }}
-                 />
-             </BannerButton>
+        <BannerButton activeOpacity={0.9} onPress={() => navegateDetailsPage(bannerMovie) }>
+          <Banner
+            resizeMethod="resize"
+            source={{
+              uri: `https://image.tmdb.org/t/p/original/${bannerMovie.poster_path}`,
+            }}
+          />
+        </BannerButton>
 
-             <SliderMovie
-               horizontal={true}
-               showsHorizontalScrollIndicator={false}
-               data={nowMovies}
-               renderItem={ ({ item }) => <SliderItem data={item}/>}
-               keyExtractor={ (item) => String(item.id) }
-             />
+        <SliderMovie
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          data={nowMovies}
+          renderItem={({ item }) => <SliderItem data={item} navigatePage={ () => navegateDetailsPage(item) } />}
+          keyExtractor={(item) => String(item.id)}
+        />
 
-             <Title>Populares</Title>
-             <SliderMovie
-               horizontal={true}
-               showsHorizontalScrollIndicator={false}
-               data={popularMovies}
-               renderItem={ ({ item }) => <SliderItem data={item}/>}
-               keyExtractor={ (item) => String(item.id) }
-             />
+        <Title>Populares</Title>
+        <SliderMovie
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          data={popularMovies}
+          renderItem={({ item }) => <SliderItem data={item} navigatePage={ () => navegateDetailsPage(item) } />}
+          keyExtractor={(item) => String(item.id)}
+        />
 
-             <Title>Mais votados</Title>
+        <Title>Mais votados</Title>
 
-             <SliderMovie
-               horizontal={true}
-               showsHorizontalScrollIndicator={false}
-               data={topMovies}
-               renderItem={ ({ item }) => <SliderItem data={item}/>}
-               keyExtractor={ (item) => String(item.id) }
-             />
-
-           </ScrollView>
-        </Container>
-    )
+        <SliderMovie
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          data={topMovies}
+          renderItem={({ item }) => <SliderItem data={item} navigatePage={ () => navegateDetailsPage(item) } />}
+          keyExtractor={(item) => String(item.id)}
+        />
+      </ScrollView>
+    </Container>
+  );
 }
 
 export default Home;
